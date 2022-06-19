3ad5927729d42c7c2ef657c3f0fa430d10760fee
Petr <petr@kle.cz>
2022-06-19 20:02:10 +0200

    Oprava manifest.json

--- icons.sh
+++ icons.sh
@@ -2,7 +2,7 @@
 
 IMGDIR=source/img/z
 
-for foo in 16x16 32x32 57x57 60x60 64x64 72x72 76x76 96x96 114x114 120x120 144x144 152x152 180x180 192x192 256x256 512x512 310x150
+for foo in 16x16 32x32 48x48 57x57 60x60 64x64 72x72 76x76 96x96 114x114 120x120 144x144 152x152 180x180 192x192 256x256 512x512 310x150
 do
 	convert $IMGDIR/zs.png -resize ${foo} -background none -gravity center -extent ${foo} $IMGDIR/zs-${foo}.png
 done
