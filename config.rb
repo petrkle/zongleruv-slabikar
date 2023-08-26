40ce7bba4f30e67afa3af6e8c52438bfd0261f5b
Petr <petr@kle.cz>
2023-08-26 12:43:35 +0200

    Formátování

--- config.rb
+++ config.rb
@@ -7,14 +7,14 @@ set :markdown, :transliterated_header_ids => true
 
 configure :development do
   config[:host] = "zongl.info"
-	activate :directory_indexes
+  activate :directory_indexes
   activate :asset_hash, :exts => %w(.css .js .json .png .svg .jpg .pdf .zip .gif .mobi .odt)
 end
 
 configure :production do
   config[:host] = "zonglovani.info"
-	activate :directory_indexes
-	activate :minify_html
+  activate :directory_indexes
+  activate :minify_html
   activate :minify_css
   activate :gzip
   activate :brotli
